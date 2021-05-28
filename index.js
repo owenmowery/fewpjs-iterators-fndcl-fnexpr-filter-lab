eb37fdb213b81a773132f8ad0b5d08693712f4df
Owen Mowery <owenmowery99@gmail.com>
2021-05-28 15:52:19 -0400

    commit tree 9d4660ace2ba45746fdd4809a19ce42bb1456a71

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    let driverMatch = drivers.filter(d => {
+        return d.toLowerCase() === name.toLowerCase();
+    });
+    return driverMatch;
+}
+
+function fuzzyMatch(drivers, string) {
+    let filterDrivers = drivers.filter(d => {
+        return d.toLowerCase().indexOf(string.toLowerCase()) === 0;
+    });
+    return filterDrivers;
+}
+
+function matchName(drivers, string) {
+    let driverMatch = drivers.filter(d => {
+        return d.name === string;
+    });
+    return driverMatch;
+}
